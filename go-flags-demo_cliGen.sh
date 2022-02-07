1ce8dfb5c9265bc9afce538fdf8afe00d8c52fa6
Tong Sun <suntong@cpan.org>
2022-02-07 13:41:24 -0500

    - [#] update _cliGen.sh for better compatibility

--- go-flags-demo_cliGen.sh
+++ go-flags-demo_cliGen.sh
@@ -1,3 +1,13 @@
- pwd=`pwd`
- cd $GOPATH/src/github.com/go-easygen/easygen/test
- easygen commandlineGoFlags.header,commandlineGoFlags.ityped.tmpl,commandlineGoFlags "$pwd/go-flags-demo"_cli | gofmt > "$pwd/go-flags-demo"_cliDef.go
+prj=go-flags-demo
+pwd=`pwd`
+srcDir=$GOPATH/src/github.com/go-easygen/easygen/test
+cd $srcDir || srcDir=/usr/share/gocode/src/github.com/go-easygen/easygen/test
+cd $srcDir || srcDir=/usr/share/doc/easygen/examples
+[ -d "$srcDir" ] || {
+  echo No template file found
+  exit 1
+}
+
+cd $srcDir
+easygen commandlineGoFlags.header,commandlineGoFlags.ityped.tmpl,commandlineGoFlags "$pwd/$prj"_cli | gofmt > "$pwd/$prj"_cliDef.go
+echo ${prj}_cliDef.go generated successful
